f6ef371274f4d8651898e74ca83cb9cbb2894e48
Mike Petrichenko <mike@btframework.com>
2025-10-04 20:46:09 +0300

    Update SmartRfSwitch.ino

--- SmartRfSwitch.ino
+++ SmartRfSwitch.ino
@@ -37,14 +37,17 @@
 bool LampState = false;
 bool CurrentLampState = false;
 
-struct RfSwitchingLamp : Service::LightBulb {
+struct RfSwitchingLamp : Service::LightBulb
+{
     SpanCharacteristic* FPower;
     
-    RfSwitchingLamp() : Service::LightBulb() {
+    RfSwitchingLamp() : Service::LightBulb()
+    {
         FPower = new Characteristic::On();
     }
     
-    bool update() {
+    bool update()
+    {
         LampState = FPower->getNewVal();
         CurrentLampState = LampState;
         digitalWrite(SWITCH_CONTROL_PIN, LampState);
@@ -60,12 +63,14 @@ struct RfSwitchingLamp : Service::LightBulb {
 
 RfSwitchingLamp* Lamp;
 
-void IRAM_ATTR SwitchSignalInterrupt() {
+void IRAM_ATTR SwitchSignalInterrupt()
+{
     LampState = !LampState;
 }
 
 // Arduino initialization routine.
-void setup() {
+void setup()
+{
     // Initialize debug serial port.
     Serial.begin(115200);
 
@@ -105,8 +110,10 @@ void setup() {
 }
 
 // Arduino main loop.
-void loop() {
-    if (CurrentLampState != LampState) {
+void loop()
+{
+    if (CurrentLampState != LampState)
+    {
         CurrentLampState = LampState;
         Lamp->FPower->setVal(CurrentLampState);
         digitalWrite(SWITCH_CONTROL_PIN, LampState ? HIGH : LOW);
