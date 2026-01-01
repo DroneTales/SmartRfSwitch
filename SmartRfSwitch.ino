09aa4d155ec22bfe208fae9f57b4a7fca8e99646
Mike Petrichenko <mike@btframework.com>
2026-01-01 14:11:56 +0300

    Update SmartRfSwitch.ino

--- SmartRfSwitch.ino
+++ SmartRfSwitch.ino
@@ -89,6 +89,9 @@ void setup()
     homeSpan.setControlPin(CONTROL_PIN);
     homeSpan.setStatusPin(STATUS_LED_PIN);
     homeSpan.setPairingCode("63145322");
+    // Enable OTA. The default OTA password is "homespan-ota"
+    homeSpan.enableOTA();
+    // Start HomeSpan.
     homeSpan.begin(Category::Lighting, "DroneTales RF Switching Lamp");
 
     // Build device's serial number.
